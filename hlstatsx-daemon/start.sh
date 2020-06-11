d2b9fd8591f64c83a92ac2fe843e4dab3c6b025b
zarlo <zarlo5899@gmail.com>
2020-06-11 19:51:59 +1000

    a

--- hlstatsx-daemon/start.sh
+++ hlstatsx-daemon/start.sh
@@ -1,7 +1,8 @@
 service cron start
 /opt/hlstatsx/GeoLiteCity/install_binary.sh >> /var/log/cron.log 2>&1
-/opt/hlstatsx/run_hlstats start >> /var/log/cron.log 2>&1
+/opt/hlstatsx/run_hlstats start >> /var/log/main.log 2>&1
 while true
 do
 	sleep 1
+    tail -f /var/log/main.log
 done
