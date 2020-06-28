3715f1a3bbddeb1dd42f4d2313ca94f9e7e1492c
zarlo <zarlo5899@gmail.com>
2020-06-28 18:08:15 +1000

    start of cert bot

--- hlstatsx-daemon/entry.sh
+++ hlstatsx-daemon/entry.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-crontab /etc/cron.d/hlstats-cron
-service cron reload
-service cron restart
-
 echo "
 AccountID $GEOIP_AccountID
 LicenseKey $GEOIP_LicenseKey
 EditionIDs GeoLite2-ASN GeoLite2-City GeoLite2-Country
 DatabaseDirectory /opt/GeoIP2-Country/" > /etc/GeoIP.conf
-
 
 find hlstats.conf -type f -exec sed -i 's/DBHost ""/DBHost "$DBHost"/g' {} \;
 find hlstats.conf -type f -exec sed -i 's/DBUsername ""/DBUsername "$DBUsername"/g' {} \;
@@ -24,7 +19,8 @@ if [ ! -f /opt/GeoIP2-Country/GeoLite2-City.mmdb ]; then
 fi
 
 ./run_hlstats start $COUNT >> /dev/stdout 2>&1
+
 while true
 do
-    sleep 1
+    sleep 10
 done
